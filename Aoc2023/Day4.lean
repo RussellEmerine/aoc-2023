1548bff6a4a8b711b9516d365787c2c542c96276
Russell Emerine <rre0520@gmail.com>
2023-12-14 23:36:32 -0500

    Day 4 task 2, ez clap

--- Aoc2023/Day4.lean
+++ Aoc2023/Day4.lean
@@ -1,6 +1,8 @@
 import Lean.Data.Json.Parser
 import Mathlib.Data.Nat.Basic
 import Mathlib.Data.List.BigOperators.Basic
+import Mathlib.Data.List.Intervals
+import Mathlib.Data.Vector.Basic 
 
 namespace Day4
 
@@ -36,7 +38,19 @@ def score' : ℕ → ℕ
 | 1 => 1
 | (n + 1) => 2 * score' n
 
-def score (c : Card) : ℕ := score' c.common 
+def score (c : Card) : ℕ := score' c.common
+
+def iterScore (a : Array Card) : ℕ := Id.run <| do
+  let mut count := Vector.replicate a.size 1
+  let mut total := 0
+  for i in List.finRange a.size do
+    let common := a[i].common
+    let current := count.get i
+    total := total + current 
+    for j in List.Ico (i + 1) (i + common + 1) do
+      if hj : j < a.size then 
+        count := count.set ⟨j, hj⟩ (count.get ⟨j, hj⟩ + current)
+  return total 
 
 end Card
 
@@ -53,10 +67,25 @@ def main : IO Unit := do
 
 end Task1
 
+namespace Task2
+
+def main : IO Unit := do
+  let lines ← IO.FS.lines (System.FilePath.mk "Data/Day4/test.txt")
+  let cards ← IO.ofExcept (lines.mapM Card.parse)
+  println! "Test: {Card.iterScore cards}"
+  println! "Expected: {30}"
+  let lines ← IO.FS.lines (System.FilePath.mk "Data/Day4/task.txt")
+  let cards ← IO.ofExcept (lines.mapM Card.parse)
+  println! "Task: {Card.iterScore cards}"
+
+end Task2
+
 def main : IO Unit := do
   println! "Day 4"
   println! "Task 1"
   Task1.main
   println! ""
+  println! "Task 2"
+  Task2.main 
 
 end Day4
