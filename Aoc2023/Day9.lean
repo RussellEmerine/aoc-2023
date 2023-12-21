9f317d505f90d4096762f9710106b5f7b44bbbb5
Russell Emerine <rre0520@gmail.com>
2023-12-21 17:06:47 -0500

    Day 9 task 2 - too easy

--- Aoc2023/Day9.lean
+++ Aoc2023/Day9.lean
@@ -26,6 +26,21 @@ def extrapolate (l : List ℤ) : ℤ :=
     l.getLast h₁ + c
 termination_by _ => l.length 
 
+def extrapolateBack (l : List ℤ) : ℤ :=
+  if h₀ : l.all (· = 0) then
+    0
+  else
+    have h₁ : l ≠ [] := by
+      by_contra h
+      subst h
+      simp at h₀
+    have : (differences l).length < l.length := by
+      rw [length_differences]
+      refine Nat.sub_lt (List.length_pos.mpr h₁) Nat.zero_lt_one
+    let c := extrapolateBack (differences l)
+    l.head h₁ - c
+termination_by _ => l.length 
+
 namespace Task1
 
 def main : IO Unit := do
@@ -39,10 +54,26 @@ def main : IO Unit := do
   
 end Task1
 
+namespace Task2
+
+def main : IO Unit := do
+  let lines ← IO.FS.lines (System.FilePath.mk "Data/Day9/test.txt")
+  let history := lines.map (List.filterMap String.toInt? <| String.splitOn ·)
+  println! "Test: {(history.map extrapolateBack).toList.sum}"
+  println! "Expected: {2}"
+  let lines ← IO.FS.lines (System.FilePath.mk "Data/Day9/task.txt")
+  let history := lines.map (List.filterMap String.toInt? <| String.splitOn ·)
+  println! "Task: {(history.map extrapolateBack).toList.sum}"
+  
+end Task2
+
 def main : IO Unit := do
   println! "Day 9"
   println! "Task 1"
   Task1.main
+  println! ""
+  println! "Task 2"
+  Task2.main
   println! ""
 
 end Day9
